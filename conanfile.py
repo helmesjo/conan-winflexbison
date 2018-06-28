32f47de814fce245f469c7f03d6e604b26d2ea6d
Fred Helmesjö <fred.helmesjo@qualisys.se>
2018-06-28 14:46:32 +0200

    Building & simple test package passing

--- conanfile.py
+++ conanfile.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    author = "Bincrafters <bincrafters@gmail.com>"
+    name = "winflexbison"
+    version = "2.5.14"
+    description =   "Win flex-bison is a windows port of \
+                    the Flex (the fast lexical analyser) and \
+                    Bison (GNU parser generator)."
+                    
+    url = "https://github.com/helmesjo/conan-winflexbison"
+    homepage = "https://sourceforge.net/projects/winflexbison/"
+    author = "helmesjo <helmesjo@gmail.com>"
     # Indicates License type of the packaged library
     license = "MIT"
 
@@ -24,8 +28,8 @@ class LibnameConan(ConanFile):
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    options = {}
+    default_options = ""
 
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
@@ -36,17 +40,16 @@ class LibnameConan(ConanFile):
     # So, with libs like zlib, updates are very rare, so we now use static version
 
 
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
+    requires = ()
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
+        else:
+            raise ConanException("{} is only supported on Windows.".format(self.name))
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/lexxmark/winflexbison"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
@@ -55,9 +58,6 @@ class LibnameConan(ConanFile):
 
     def configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
         cmake.configure(build_folder=self.build_subfolder)
         return cmake
 
@@ -67,17 +67,13 @@ class LibnameConan(ConanFile):
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+
+        bison_folder = os.path.join(self.source_subfolder, "bison")
+        self.copy(pattern="data/*", dst="bin", src=bison_folder)
+        self.copy(pattern="*.exe", dst="bin", keep_path=False)
+        self.copy(pattern="*.h", dst="include", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, "include"))
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,27 @@
-#include <cstdlib>
+#include <cstdio>
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Bincrafters\n";
+extern "C" int yylex();
+extern "C" int yyparse();
+extern FILE *yyin;
+
+int main(int, char**) {
+    const auto file = "parseme.txt";
+	// open a file handle to a particular file:
+	FILE *myfile = fopen(file, "r");
+	// make sure it is valid:
+	if (!myfile) {
+		cout << "I can't open " << file << "!" << endl;
+		return -1;
+	}
+	// set flex to read from it instead of defaulting to STDIN:
+	yyin = myfile;
+	
+	// parse through the input until there is no more:
+	do {
+		yyparse();
+	} while (!feof(yyin));
+	
     return EXIT_SUCCESS;
 }
